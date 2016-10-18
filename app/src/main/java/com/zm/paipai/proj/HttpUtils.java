68a2a7f54df8de65e4784dbba69be2c39efbd9b7
wsd325888 <wang325888>
2016-10-18 11:10:23 +0800

    author:wang desc:修改网络地址

--- app/src/main/java/com/zm/paipai/proj/HttpUtils.java
+++ app/src/main/java/com/zm/paipai/proj/HttpUtils.java
@@ -4,5 +4,5 @@ package com.zm.paipai.proj;
  * Created by Administrator on 2016/9/30.
  */
 public class HttpUtils {
-    public static final String host="http://10.40.5.23:8080/webpro4/getPath";
+    public static final String host="http://10.40.5.24:8080/webpro4/getPath";
 }
